572ecb9dac6d79a4212c16d8c9e1ecc9c3f708b9
hongsam14 <69339846+hongsam14@users.noreply.github.com>
2021-08-15 04:23:30 +0900

    2021.08.16.04.23 리스트 슬라이싱.
    [:3] -> 처음부터 3번째 인덱스까지
    [:] ->전체
    [1:3]-> 인덱스 1~3까지
    [7:] -> 7부터
    
    [-1] -> 마지막 인덱스
    [-1::-1] -> 리스트 역순
    [::-1] -> "

--- chapter_1/2588_곱셈.py
+++ chapter_1/2588_곱셈.py
@@ -1,18 +1,11 @@
 #! /usr/bin/python3
 
 def	main():
-	out = []
-	result = 0;
-	dest = input()
+	dest = int(input())
 	src = input()
-	for i in map(lambda i: int(dest) * int(i), src):
-		out.insert(0, i)
-	
-	for i, value in enumerate(out):
-		print(value)
-		result += (10 ** i) * value
-	print(result)
-
+	for i in list(map(lambda i: dest * int(i), src))[::-1]:
+		print(i)
+	print(dest * int (src))
 
 if __name__ == '__main__':
 	main()
